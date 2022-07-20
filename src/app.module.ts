4e101d326c0f1b5eb55548206e4649d7b328a0d0
flopez10 <fabio.a.lopez@outlook.com>
2022-07-19 20:45:00 -0500

    auth pt 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
+console.log(join(__dirname, '**', '*.entity.{ts,js}'));
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { TasksController } from './tasks.controller';
+import { TasksRepository } from './tasks.repository';
+import { TasksService } from './tasks.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule],
+  controllers: [TasksController],
+  providers: [TasksService],
+})
 export class TasksModule {}
